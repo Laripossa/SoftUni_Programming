8dee1b605bdf3a6fdf9e6ca7f78471ed4856d4a1
Laripossa <laratraykova03@gmail.com>
2023-10-15 20:12:08 +0100

    sum_of_beach solved

--- basic_syntax_more_exercises/sum_of_a_beach.py
+++ basic_syntax_more_exercises/sum_of_a_beach.py
@@ -1,51 +1,11 @@
-received_word = input()
-letters_in_the_word = []
-sand = ["S", "a", "n", "d"]
-water = ["W", "a", "t", "e", "r"]
-fish = ["F", "i", "s", "h"]
-sun = ["S", "u", "n"]
-times_count = 0
+input_word = input()
 
-for char in received_word:
-    letters_in_the_word.append(char)
+target_words = ["sand", "water", "fish", "sun"]
+counter = 0
 
-current_letter = letters_in_the_word
+for word in target_words:
+    count = input_word.lower().count(word)
+    counter += count
 
-for sand_char in letters_in_the_word:
-    if sand_char in sand:
-        sand.remove(sand_char)
-
-    if not sand:
-        times_count += 1
-        letters_in_the_word = current_letter
-        break
-
-for water_char in letters_in_the_word:
-    if water_char in water:
-        water.remove(water_char)
-
-    if not water:
-        times_count += 1
-        letters_in_the_word = current_letter
-        break
-
-for fish_char in letters_in_the_word:
-    if fish_char in fish:
-        fish.remove(fish_char)
-
-    if not fish:
-        times_count += 1
-        letters_in_the_word = current_letter
-        break
-
-for sun_char in letters_in_the_word:
-    if sun_char in sun:
-        sun.remove(sun_char)
-
-    if not sun:
-        times_count += 1
-        letters_in_the_word = current_letter
-        break
-
-print(times_count)
+print(counter)
 
